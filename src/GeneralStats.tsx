ef2fdc1d2455a20ab5953fcc30bb348a0d41d5e4
Brendan Fahy <bmfahy@gmail.com>
2022-03-08 03:02:54 -0500

    add bar charts

--- src/GeneralStats.tsx
+++ src/GeneralStats.tsx
@@ -1,10 +1,15 @@
+import { Stack } from '@devexpress/dx-react-chart';
+import {
+  ArgumentAxis, BarSeries, Chart, ValueAxis
+} from '@devexpress/dx-react-chart-material-ui';
+import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import DisplayGeneral from "./Generals";
 import { General, GeneralStat, GeneralStats } from "./proto/match";
@@ -25,30 +30,47 @@ function getGeneralStats(callback: (m: GeneralStats) => void) {
 }
 
 function DisplayGeneralStat(props: { stat: GeneralStat }) {
-  const sorted = props.stat.stats.sort((s1,s2)=> s1.playerName.length - s2.playerName.length)
-    const overall = props.stat.total
-    return (
-    <Paper>
-      <List>
-        <ListItem>
-          <ListItemAvatar>
-            <DisplayGeneral general={props.stat.general} />
-          </ListItemAvatar>
-          <ListItemText primary={General[props.stat.general]} />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary={`Total: (${overall?.wins ?? 0} : ${overall?.losses ?? 0}) `} />
-        </ListItem>
-        <Divider />
-        {sorted.map(p =>
-        (
-          <ListItem key={p.playerName}>
-            <ListItemText key={p.playerName + "-text"} primary={`${p.playerName}: (${p.winLoss?.wins ?? 0} : ${p.winLoss?.losses ?? 0})`} />
-          </ListItem>
-        )
-        )}
-      </List>
-    </Paper >
+  const sorted = props.stat.stats.sort((s1, s2) => s1.playerName.length - s2.playerName.length)
+  const overall = props.stat.total
+  let data = sorted.map(s => ({ name: s.playerName, wins: s.winLoss?.wins ?? 0, losses: s.winLoss?.losses ?? 0 }))
+  data = [{ name: "overall", wins: overall?.wins ?? 0, losses: overall?.losses ?? 0 }, ...data]
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2} m={1}>
+        <Grid item xs={2}>
+
+          <List>
+            <ListItem>
+              <ListItemAvatar>
+                <DisplayGeneral general={props.stat.general} />
+              </ListItemAvatar>
+              <ListItemText primary={General[props.stat.general]} />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary={`Total: (${overall?.wins ?? 0} : ${overall?.losses ?? 0}) `} />
+            </ListItem>
+            <Divider />
+            {sorted.map(p =>
+            (
+              <ListItem key={p.playerName}>
+                <ListItemText key={p.playerName + "-text"} primary={`${p.playerName}: (${p.winLoss?.wins ?? 0} : ${p.winLoss?.losses ?? 0})`} />
+              </ListItem>
+            )
+            )}
+          </List>
+        </Grid>
+        <Grid item xs={9}>
+          <Chart data={data}>
+            <ArgumentAxis />
+            <ValueAxis />
+            <BarSeries valueField="wins" argumentField="name" name="wins" />
+            <BarSeries valueField="losses" argumentField="name" name="losses" />
+            <Stack />
+          </Chart>
+
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
@@ -63,7 +85,6 @@ export default function DisplayGeneralStats() {
   return (<Paper>
 
     {/* <Button variant="contained" onClick={() => getGeneralStats(setGeneralStats)} >Get Matches</Button> */}
-    <Typography>Will make this graphs later.</Typography>
     {generalStats.generalStats.map(m => (<><DisplayGeneralStat stat={m} /><Divider /></>))}
   </Paper>);
 }
--- src/PlayerStats.tsx
+++ src/PlayerStats.tsx
@@ -1,16 +1,18 @@
+import { Stack } from '@devexpress/dx-react-chart';
+import {
+  ArgumentAxis, BarSeries, Chart, ValueAxis
+} from '@devexpress/dx-react-chart-material-ui';
+import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import DisplayGeneral from "./Generals";
 import { General, PlayerStat, PlayerStats } from "./proto/match";
-
-
-
 
 function getPlayerStats(callback: (m: PlayerStats) => void) {
   fetch("/api/playerstats",
@@ -24,25 +26,39 @@ function getPlayerStats(callback: (m: PlayerStats) => void) {
 }
 
 function DisplayPlayerStat(props: { stat: PlayerStat }) {
-  const sorted = props.stat.stats.sort((s1,s2)=> s1.general - s2.general)
+  const sorted = props.stat.stats.sort((s1, s2) => s1.general - s2.general)
+  const data = sorted.map(p => ({ general: General[p.general], wins: p.winLoss?.wins ?? 0, losses: p.winLoss?.losses ?? 0 }))
   return (
-    <Paper>
-      <List>
-        <ListItem>
-          <ListItemText primary={"Player Name: " + props.stat.playerName} />
-        </ListItem>
-        {sorted.map(p =>
-        (
-          <ListItem>
-            <ListItemAvatar>
-              <DisplayGeneral general={p.general} />
-            </ListItemAvatar>
-            <ListItemText primary={`${General[p.general]}: (${p.winLoss?.wins ?? 0} : ${p.winLoss?.losses ?? 0})`} />
-          </ListItem>
-        )
-        )}
-      </List>
-    </Paper >
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={2}>
+            <List>
+              <ListItem>
+                <ListItemText primary={"Player Name: " + props.stat.playerName} />
+              </ListItem>
+              {sorted.map(p =>
+              (
+                <ListItem>
+                  <ListItemAvatar>
+                    <DisplayGeneral general={p.general} />
+                  </ListItemAvatar>
+                  <ListItemText primary={`${General[p.general]}: (${p.winLoss?.wins ?? 0} : ${p.winLoss?.losses ?? 0})`} />
+                </ListItem>
+              )
+              )}
+            </List>
+          </Grid>
+          <Grid item xs={9}>
+            <Chart data={data}>
+              <ArgumentAxis />
+              <ValueAxis />
+              <BarSeries valueField="wins" argumentField="general" name="wins" />
+              <BarSeries valueField="losses" argumentField="general" name="losses" />
+              <Stack />
+            </Chart>
+          </Grid>
+        </Grid>
+      </Box>
   )
 }
 
@@ -57,7 +73,6 @@ export default function DisplayPlayerStats() {
   return (<Paper>
 
     {/* <Button variant="contained" onClick={() => getPlayerStats(setPlayerStats)} >Get Matches</Button> */}
-    <Typography>Will make this graphs later.</Typography>
     {playerStats.playerStats.map(m => (<><DisplayPlayerStat stat={m} /><Divider /></>))}
   </Paper>);
 }
