3ec8ad44b67cb64a2cc3291562187576b3caf192
Brendan Fahy <bmfahy@gmail.com>
2022-04-01 03:21:50 -0400

    fetch from s3 in parallel

--- backend/data/save.go
+++ backend/data/save.go
@@ -5,38 +5,45 @@ import (
 
 	pb "github.com/f4hy/generals-stats/backend/proto"
 	s3 "github.com/f4hy/generals-stats/backend/s3"
-	"github.com/golang/protobuf/proto"
 	log "github.com/golang/glog"
+	"github.com/golang/protobuf/proto"
 )
 
 func SaveMatch(match *pb.MatchInfo) error {
-        path := fmt.Sprintf("matches/%d.json", match.GetId())
-        dataToSave, err := proto.Marshal(match)
+	path := fmt.Sprintf("matches/%d.json", match.GetId())
+	dataToSave, err := proto.Marshal(match)
 	if err != nil {
 		return err
 	}
 	log.Infof("Saving match to %s", path)
-        err = s3.AddDataToS3(path, dataToSave)
+	err = s3.AddDataToS3(path, dataToSave)
 	return err
 }
 
+func getMatch(c chan *pb.MatchInfo, matchpath string) {
+	log.Infof("fetching match %s", matchpath)
+	matchdata, _ := s3.GetS3Data(matchpath)
+	match := &pb.MatchInfo{}
+	proto.Unmarshal(matchdata, match)
+	c <- match
+}
 
 func GetMatches() (*pb.Matches, error) {
-	listing, err :=s3.List("matches/")
+	listing, err := s3.List("matches/")
 	log.Infof("Found %d matchs", len(listing))
-	if err!=nil{
+	if err != nil {
 		return nil, err
 	}
-	var matches  []*pb.MatchInfo
+	var matches []*pb.MatchInfo
+	c := make(chan *pb.MatchInfo)
 	for _, matchpath := range listing {
-		log.Infof("fetching match %s", matchpath)
-		matchdata, err := s3.GetS3Data(matchpath)
-		if err!=nil{
-			return nil,err
-		}
-		match :=&pb.MatchInfo{};
-		proto.Unmarshal(matchdata, match)
-		matches = append(matches, match)		
+		// go routines are nice!
+		go getMatch(c, matchpath)
+	}
+	for i := range listing {
+		log.Infof("recieving match %d", i)
+		match := <-c
+		matches = append(matches, match)
 	}
 	return &pb.Matches{
 		Matches: matches,
