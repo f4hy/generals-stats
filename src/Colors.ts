74db2e00b8bb1d9ca9ccd0dec0022151c2cbff51
Brendan Fahy <bmfahy@gmail.com>
2022-05-10 00:03:56 -0400

    add missing colors

---
+++ src/Colors.ts
@@ -0,0 +1,18 @@
+export function TeamColor(name: string) {
+  switch (name) {
+    case "1":
+    case "Modus":
+    case "Brendan":
+    case "jbb":
+    case "Jared":
+      return "#82ca9d"
+    case "3":
+    case "OneThree111":
+    case "Bill":
+    case "Ye_Ole_Seans":
+    case "Sean":
+      return "#8884d8"
+    default:
+      return "#444444"
+  }
+}
