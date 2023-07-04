09c2b1d535dd984342148b2942331dd1fc32fdb7
Brendan Fahy <bmfahy@gmail.com>
2023-07-04 13:19:36 -0400

    mark a missmatch

--- backend/data/cncstats_parser.go
+++ backend/data/cncstats_parser.go
@@ -355,6 +355,7 @@ func knownAborted(matchId int64) string {
 		1349671078: "Missmatch at start ",
 		3754206351: "Missmatch :( ",
 		2606388828: "Disconnect",
+		2733966289: "Missmatch at end :("
 	}
 	reason := aborted[matchId]
 	return reason
